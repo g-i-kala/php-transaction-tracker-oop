69973e8d15b8cb6a9bf182efdd9447c52507e61f
g-i-kala <karocreativedesigns@gmail.com>
2025-05-12 12:01:37 +0200

    refactor: FIleUploader uploads the file

--- app/Controllers/UploadController.php
+++ app/Controllers/UploadController.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use App\UploadValidator;
 use App\View;
+use App\FileUploader;
+use App\UploadValidator;
 
 class UploadController
 {
@@ -29,44 +30,8 @@ class UploadController
             return View::make('index', ['errors' => $errors]);
         }
 
-
-
-
-
-        // if (!isset($_FILES["myFile"])) {
-        //     dd("There is no file to upload.");
-        // }
-
-        // $filepath = $_FILES['myFile']['tmp_name'];
-        // $fileSize = filesize($filepath);
-        // $fileinfo = finfo_open(FILEINFO_MIME_TYPE);
-        // $filetype = finfo_file($fileinfo, $filepath);
-
-        // if ($fileSize === 0) {
-        //     dd("The file is empty.");
-        // }
-
-        // if ($fileSize > 3145728) { // 3 MB (1 byte * 1024 * 1024 * 3 (for 3 MB))
-        //     dd("The file is too large");
-        // }
-
-
-
-        // if (!in_array($filetype, array_keys($allowedTypes))) {
-        //     dd("File not allowed.");
-        // }
-
-        // $filename = basename($filepath);
-        // $extension = $allowedTypes[$filetype];
-        // $targetDirectory = STORAGE_PATH . "/uploads";
-
-        // $newFilepath = $targetDirectory . "/" . $filename . "." . $extension;
-
-        // if (!move_uploaded_file($filepath, $newFilepath)) {
-        //     dd("Can't move file.");
-        // }
-
-        // echo "File uploaded successfully :)";
+        $uploader = new FileUploader($file['tmp_name']);
+        $path = $uploader->moveTo(STORAGE_PATH . "/uploads");
 
         // $data = readFile($file);
         // storeDataInDB = Transactions::create($data);
