24fdc57b310237d9291d908f391253ad773d13e2
g-i-kala <5star@wp.pl>
2025-06-11 09:19:48 +0200

    refactor: error & validation handling

---
+++ app/Forms/UploadForm.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Forms;
+
+class UploadForm
+{
+    protected $errors = [];
+
+    public function __construct(private $file)
+    {
+        //
+    }
+
+
+
+
+}
